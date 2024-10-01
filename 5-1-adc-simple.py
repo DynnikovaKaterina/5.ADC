44e872fd96b08e8c101e6386e506051d1fa21d7c
DynnikovaKaterina <dynnikova.ki@phystech.edu>
2024-10-01 16:40:17 +0300

    Update 5-1-adc-simple.py

--- 5-1-adc-simple.py
+++ 5-1-adc-simple.py
@@ -1,25 +1,38 @@
 import RPi.GPIO as gp
+import time
 dac = [8, 11, 7, 1, 0, 5, 12, 6]
 gp.setmode(gp.BCM) 
 gp.setup(dac, gp.OUT) #настраиваем все пины из списка dac на выход
-comp = 
-troyka = 
+comp = 14
+troyka = 13
+
 
 #функция перевода из десятичного числа в двоичное 
 def decimal2binary(value):
     return [int(bit) for bit in bin(value)[2:].zfill(8)]
 
-#функция возвращает десятичное число, пропорциональное напряжению клемме S тройка-модуля
-def adc():
-    return( gp.input(troyka) )
+
+def adc(v):
+    signal = decimal2binary(v)
+    gp.output(dac, signal)
+    return signal
 
 
-gp.setup(troyka, gp.OUT, initial= )
+gp.setup(troyka, gp.OUT, initial= gp.HIGH)
 gp.setup(comp, gp.IN)
 
 try:
-    adc()
+    while True:
+        for i in range (256):
+            signal = adc(i)
+            time.sleep(0.001)
+            U = 3.3 * i / 256
+            compVal = gp.input(comp)
+            if compVal == 1:
+                print("цифровое значение:", i, ", напряжение: ", U)
+                break
 
 finally:
     gp.output(dac, 0)
+    gp.output(troyka, 0)
     gp.cleanup()
