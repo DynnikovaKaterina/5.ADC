9d91f63b7ef8e0804ea48417b081d95e610e5d40
DynnikovaKaterina <dynnikova.ki@phystech.edu>
2024-10-04 21:59:33 +0300

    5-3-preliminary-version

---
+++ 5-3-adc-volume.py
@@ -0,0 +1,48 @@
+import RPi.GPIO as gp
+import time
+dac = [8, 11, 7, 1, 0, 5, 12, 6]
+leds = [2, 3, 4, 17, 27, 22, 10, 9]
+gp.setmode(gp.BCM) 
+gp.setup(dac, gp.OUT) #настраиваем все пины из области dac на выход
+gp.setup(leds, gp.OUT) #настраиваем все пины из области leds на выход
+comp = 14
+troyka = 13
+
+#функция перевода из десятичного числа в двоичное 
+def decimal2binary(value):
+    return [int(bit) for bit in bin(value)[2:].zfill(8)]
+
+def slow_adc():
+    for i in range(256):
+        gp.output(dac, decimal2binary(i))
+        time.sleep(0.01)
+        compVal = gp.input(comp)
+        if compVal == 1:
+            return i
+    return 255
+
+def adc():
+    i0 = 0
+    for i in range (7, -1, -1):
+        gp.output(dac, decimal2binary(i0 + 2**i))
+        time.sleep(0.001)
+        compVal = gp.input(comp)
+        if compVal == 0:
+            i0 += 2**i
+    return i0
+
+gp.setup(troyka, gp.OUT, initial= gp.HIGH)
+gp.setup(comp, gp.IN)
+
+try:
+    while True:
+        blinding_lights = (adc() + 1) // 32
+        for i in range(blinding_lights):
+            gp.output(leds[i], 1)
+        for i in range(blinding_lights, 8):
+            gp.output(leds[i], 0)
+
+finally:
+    gp.output(dac, 0)
+    gp.output(troyka, 0)
+    gp.cleanup()
